98fadb6ce9a2d070bee62c9ac6929644cee7de1d
Ed Crewe <ed.crewe@oracle.com>
2019-05-07 07:34:17 +0100

    update history and version

--- python/fr-helm-celery/HISTORY.rst
+++ python/fr-helm-celery/HISTORY.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+0.0.2 Unreleased
+----------------
+
+- Completed Demo Python version
+
 0.0.1 - Unreleased
 ------------------
 
--- python/fr-helm-celery/setup.py
+++ python/fr-helm-celery/setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "0.0.1"
+version = "0.0.2"
 
 with open("README.rst", "r") as fp:
     description = fp.read() + "\n"
